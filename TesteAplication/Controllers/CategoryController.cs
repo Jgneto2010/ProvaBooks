32988539eddfa51113d014098407b5599bd1a77a
João Garcia Neto <jgneto2010@hotmail.com>
2020-03-03 15:05:54 -0300

    Adicionando Controller Add Category

---
+++ TesteAplication/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TesteAplication.ViewerModel;
+
+namespace TesteAplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpPost]
+        [Route("addCategory")]
+        public async Task<IActionResult> Post([FromServices]ICategoryRepository repositorio, [FromBody]AddCategorysModels categoryModel)
+        {
+
+            var prod = new Category(categoryModel.Name);
+            
+            await repositorio.Add(prod);
+            await repositorio.SaveChanges();
+            return Created($"api/aplicacao/{prod.Name}", new { prod.Id });
+        }
+    }
+}
---
+++ TesteAplication/ViewerModel/AddCategorysModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteAplication.ViewerModel
+{
+    public class AddCategorysModels
+    {
+        public string Name { get; set; }
+    }
+}
