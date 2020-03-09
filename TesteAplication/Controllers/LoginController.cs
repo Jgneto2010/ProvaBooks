dd8428b55b63715716b4eabd0e3756595e395ee0
João Garcia Neto <jgneto2010@hotmail.com>
2020-03-09 13:49:05 -0300

    Teste

--- TesteAplication/Controllers/LoginController.cs
+++ TesteAplication/Controllers/LoginController.cs
@@ -1,14 +1,21 @@
 ﻿using Domain.Entities;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using TesteAplication.Security;
 using TesteAplication.ViewerModel;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiUsuarios.Models;
 
 namespace TesteAplication.Controllers
 {
@@ -20,7 +27,7 @@ namespace TesteAplication.Controllers
         [HttpPost]
         public object Post(
             [FromBody]AccessCredentials credenciais,
-            [FromServices]AccessManager accessManager)
+        [FromServices]AccessManager accessManager)
         {
             if (accessManager.ValidateCredentials(credenciais))
             {
@@ -38,45 +45,22 @@ namespace TesteAplication.Controllers
 
         [HttpPost]
         [Route("registerUser")]
-        public async Task<IActionResult> Register([FromServices]UserManager<ApplicationUser> userManager, [FromBody] RegisterUser registerUser, RegisterPasswordUser registerPasswordUser)
+        public async Task<ActionResult> CreateUser([FromServices]UserManager<ApplicationUser> userManager, [FromBody] RegisterUser registerUser, RegisterPasswordUser registerPasswordUser)
         {
-
-            if (ModelState.IsValid)
+            var user = new ApplicationUser(registerUser.FirstName, registerUser.LastName, registerUser.Telephone, registerUser.Email, registerUser.Address);
+            var result = await userManager.CreateAsync(user, registerPasswordUser.Password);
+           
+            if (result.Succeeded)
             {
-
-                var user = new ApplicationUser(registerUser.FirstName, registerUser.LastName, registerUser.Telephone, registerUser.Email, registerUser.Address);
-
-                var result = await userManager.CreateAsync(user, registerPasswordUser.Password);
-
-                if (result.Succeeded)
-
-                {
-
-                    await SignInAsync(user, isPersistent: false);
-
-                    return RedirectToAction("Index", "Home");
-
-                }
-
-                else
-
-                {
-
-                    AddErrors(result);
-
-                }
-
+                return userManager.CreateSecurityTokenAsync(user);
             }
-
-
-
-
-            return View(model);
-
-
-
-
-
+            else
+            {
+                return BadRequest("Usuário ou senha inválidos");
+            }
         }
+
+
     }
 }
+
